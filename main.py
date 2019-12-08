feb486ebb52b25b9c38cc5b81ae3b8457f8c464e
bienking <56889851+bien-king@users.noreply.github.com>
2019-12-08 19:55:58 +0000

    Item 1,2
    Assigned to Bien

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-test
+import requests
+import random
+from pprint import pprint
+
+def generate_rand_int(min,max):
+    random_int = random.randint(min,max)
+    return random_int
+
+def get_characters():
+    url = "http://hp-api.herokuapp.com/api/characters"
+    response = requests.get(url)
+    characters = response.json()
+    return characters
+
+def get_random_character():
+    my_int = generate_rand_int(min=0, max=24)
+    characters = get_characters()
+    random_character = characters[my_int]
+    pprint("User ID: %d" % my_int)
+    pprint(random_character)
+
+get_random_character()
+
